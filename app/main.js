d3aa1a7e78e974fea5a46bd725a228063535cd5a
Guerson  Rodriguez <guerson.rodriguez@outlook.com>
2024-08-28 07:07:37 +0000

    ScrollReveal-Animations

--- app/main.js
+++ app/main.js
@@ -13,7 +13,7 @@ const showMenu = (toggleId, navId) => {
 
 showMenu("nav-toggle", "nav-menu");
 
-  // Close Menu
+/*=============== Close MENU ===============*/
 const navClose = document.getElementById("nav-close");
 
 if (navClose) {
@@ -87,3 +87,24 @@ const scrollActive = () =>{
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+    reset: true 
+    // reset: true // Animations repeat
+})
+
+sr.reveal(`.home__data, .explore__data, .footer__container`)
+sr.reveal(`.home__card`, {delay: 600, distance: '100px', interval: 100})
+sr.reveal(`.about__data, .join__image`, {origin: 'right'})
+sr.reveal(`.about__image, .join__data`, {origin: 'left'})
+sr.reveal(`.popular__card`, {interval: 200})
+sr.reveal(`.video-card`, {delay: 600, distance: '100px', interval: 100})
+
+
+
+
